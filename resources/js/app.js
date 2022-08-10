4550d1f82fc9905b38262871836cb0853412e1e1
Ménkű Ottó <ottomenku@gmail.com>
2022-08-10 20:03:11 +0200

    vue dev 1

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,26 @@
-import './bootstrap';
+//require('./bootstrap');
+
+import Vue from 'vue';
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+Vue.use(VueAxios, axios);
+
+import App from './App.vue';
+import Example from './components/Example.vue';
+
+const routes = [
+  {
+      name: 'Example',
+      path: '/',
+      component: Example
+  }
+];
+
+const router = new VueRouter({ mode: 'history', routes: routes});
+new Vue(Vue.util.extend({ router }, App)).$mount('#app');
+
+
